05ff19cfc1b2ba141d8009a3a286e9decae3bb28
Mithra Perera <mithraperera@hotmail.com>
2023-04-23 19:46:45 -0400

    eqArrays function completed and tested.

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return true;
+}
+
+//TEST CASES
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays([1, 2], [3, 2, 1]), false);
+assertEqual(eqArrays([], []), true); // => should PASS
