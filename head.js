3b0d9416f2f56c475e10462dda91f01827a58568
Mithra Perera <mithraperera@hotmail.com>
2023-05-13 17:55:27 -0400

    assertEqual and head refactored and tests moved to test folder.

--- head.js
+++ head.js
@@ -1,17 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
 function head(arr) {
   return arr[0];
 }
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
-assertEqual(head(['Head', 'LHL']), 'Head');
+module.exports = head;
